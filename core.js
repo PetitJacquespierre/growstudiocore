9fa9869199adfc4da46480e94a364e67264b354f
PetitJacquespierre <petitjacquespierre@gmail.com>
2026-08-18 20:28:16 -0400

    Add files via upload

--- core.js
+++ core.js
@@ -731,3 +731,44 @@ window.addEventListener('appinstalled', (evt) => {
     if (installBtn) installBtn.style.display = 'none';
 });
 
+
+
+// =========================================
+// DRAG TO SCROLL (Para PC)
+// =========================================
+function enableDragToScroll(selector) {
+    document.querySelectorAll(selector).forEach(slider => {
+        let isDown = false;
+        let startX;
+        let scrollLeft;
+        slider.addEventListener('mousedown', (e) => {
+            isDown = true;
+            slider.classList.add('active');
+            startX = e.pageX - slider.offsetLeft;
+            scrollLeft = slider.scrollLeft;
+        });
+        slider.addEventListener('mouseleave', () => {
+            isDown = false;
+            slider.classList.remove('active');
+        });
+        slider.addEventListener('mouseup', () => {
+            isDown = false;
+            slider.classList.remove('active');
+        });
+        slider.addEventListener('mousemove', (e) => {
+            if(!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - slider.offsetLeft;
+            const walk = (x - startX) * 2;
+            slider.scrollLeft = scrollLeft - walk;
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        enableDragToScroll('.upsell-container');
+        enableDragToScroll('.filters-container');
+        enableDragToScroll('.promos-container');
+    }, 1500);
+});
